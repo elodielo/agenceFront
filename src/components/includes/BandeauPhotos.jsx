42916936db6a6a6521350724e1705ce09efe446e
Elodie <elodie.loeb.simplon@gmail.com>
2024-05-16 13:51:09 +0200

    lien img

--- src/components/includes/BandeauPhotos.jsx
+++ src/components/includes/BandeauPhotos.jsx
@@ -28,7 +28,7 @@ export default function BandeauPhotos(props) {
                     width={200}
                     height={200}
                     src={
-                      "https://simplondevgrenoble.nohost.me/elodiel/agenceVoyage/public/images/" + voyage.endroit.lienImage
+                      "https://simplondevgrenoble.nohost.me/elodiel/agenceVoyage/images/" + voyage.endroit.lienImage
                     }
                   />
                   {/* <p> image:"http://127.0.0.1:8000/images/" +{voyage.endroit.lienImage} </p> */}
